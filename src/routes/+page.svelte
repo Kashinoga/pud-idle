bd7b2f0dd9fbe244d55fdd29d3d1a78e176eb7ea
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2025-11-30 17:28:23 -0600

    added icon source

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -329,14 +329,199 @@
 				</div>
 				<div class="controls">
 					<button onclick={() => (isPaused = !isPaused)} class="button-small">
-						{isPaused ? '▶ Play' : '⏸️ Pause'}
+						{#if isPaused}
+							<svg
+								width="1em"
+								height="1em"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								stroke-width="1"
+								style="display: inline-block; margin-right: 0.5em;"
+								><path
+									d="M6.90588 4.53682C6.50592 4.2998 6 4.58808 6 5.05299V18.947C6 19.4119 6.50592 19.7002 6.90588 19.4632L18.629 12.5162C19.0211 12.2838 19.0211 11.7162 18.629 11.4838L6.90588 4.53682Z"
+									fill="currentColor"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path></svg
+							>
+						{:else}
+							<svg
+								width="1em"
+								height="1em"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								stroke-width="1"
+								style="display: inline-block; margin-right: 0.5em;"
+								><path
+									d="M6 18.4V5.6C6 5.26863 6.26863 5 6.6 5H9.4C9.73137 5 10 5.26863 10 5.6V18.4C10 18.7314 9.73137 19 9.4 19H6.6C6.26863 19 6 18.7314 6 18.4Z"
+									fill="currentColor"
+									stroke="currentColor"
+									stroke-width="1"
+								></path><path
+									d="M14 18.4V5.6C14 5.26863 14.2686 5 14.6 5H17.4C17.7314 5 18 5.26863 18 5.6V18.4C18 18.7314 17.7314 19 17.4 19H14.6C14.2686 19 14 18.7314 14 18.4Z"
+									fill="currentColor"
+									stroke="currentColor"
+									stroke-width="1"
+								></path></svg
+							>
+						{/if}
+						{isPaused ? 'Play' : 'Pause'}
 					</button>
-					<button class="button-small" onclick={ToggleTheme}
-						>{isDarkMode ? '☀️ Light' : '🌙 Dark'}</button
-					>
-					<button onclick={ResetGame} class="button-small">🔄 Reset</button>
+					<button class="button-small" onclick={ToggleTheme}>
+						{#if isDarkMode}
+							<svg
+								width="1em"
+								height="1em"
+								stroke-width="1"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								color="currentColor"
+								><path
+									d="M21 2L20 3"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M3 2L4 3"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M21 16L20 15"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M3 16L4 15"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M9 18H15"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M10 21H14"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M11.9998 3C7.9997 3 5.95186 4.95029 5.99985 8C6.02324 9.48689 6.4997 10.5 7.49985 11.5C8.5 12.5 9 13 8.99985 15H14.9998C15 13.0001 15.5 12.5 16.4997 11.5001L16.4998 11.5C17.4997 10.5 17.9765 9.48689 17.9998 8C18.0478 4.95029 16 3 11.9998 3Z"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path></svg
+							>
+						{:else}
+							<svg
+								width="1em"
+								height="1em"
+								stroke-width="1"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								color="currentColor"
+								><path
+									d="M9 18H15"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M10 21H14"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M16.4999 11.5C17.4997 10.5 17.9765 9.48689 17.9999 8C18.0479 4.95029 16 3 11.9999 3C10.8324 3 9.83119 3.16613 8.99988 3.47724"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M8.99985 15C9 13 8.5 12.5 7.49985 11.5C6.4997 10.5 6.02324 9.48689 5.99985 8C5.99142 7.46458 6.0476 6.96304 6.1676 6.5"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path><path
+									d="M3 3L21 21"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path></svg
+							>
+						{/if}
+						{isDarkMode ? 'Light' : 'Dark'}
+					</button>
+					<button onclick={ResetGame} class="button-small">
+						<svg
+							width="1em"
+							height="1em"
+							viewBox="0 0 24 24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							color="currentColor"
+							stroke-width="1"
+							><path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M1.25 12C1.25 6.06294 6.06294 1.25 12 1.25C17.9371 1.25 22.75 6.06294 22.75 12C22.75 17.9371 17.9371 22.75 12 22.75C6.06294 22.75 1.25 17.9371 1.25 12ZM11.9877 7.75C9.70121 7.75 7.9471 9.28219 7.74541 11.0835C7.69932 11.4951 7.32825 11.7914 6.91661 11.7453C6.50497 11.6992 6.20863 11.3282 6.25472 10.9165C6.556 8.22597 9.07599 6.25 11.9877 6.25C13.6578 6.25 15.1863 6.8937 16.2503 7.94689V7.5C16.2503 7.08579 16.5861 6.75 17.0003 6.75C17.4145 6.75 17.7503 7.08579 17.7503 7.5V9.12222C17.7503 9.86781 17.1459 10.4722 16.4003 10.4722H14.4941C14.0799 10.4722 13.7441 10.1364 13.7441 9.72222C13.7441 9.30801 14.0799 8.97222 14.4941 8.97222H15.1523C14.3818 8.23175 13.2617 7.75 11.9877 7.75ZM12.0123 16.25C14.158 16.25 16.03 14.4222 16.2529 11.9331C16.2899 11.5205 16.6543 11.216 17.0669 11.253C17.4794 11.2899 17.7839 11.6544 17.747 12.0669C17.467 15.1926 15.0648 17.75 12.0123 17.75C10.3037 17.75 8.79345 16.943 7.74967 15.6877V16.4004C7.74967 16.8146 7.41389 17.1504 6.99967 17.1504C6.58546 17.1504 6.24967 16.8146 6.24967 16.4004V14.2226C6.24967 13.477 6.85409 12.8726 7.59967 12.8726H9.50586C9.92007 12.8726 10.2559 13.2084 10.2559 13.6226C10.2559 14.0368 9.92007 14.3726 9.50586 14.3726H8.63282C9.42384 15.5314 10.6601 16.25 12.0123 16.25Z"
+								fill="currentColor"
+							></path></svg
+						>
+						Reset
+					</button>
 					<button onclick={toggleAllSections} class="toggle-all-button button-small">
-						{allCollapsed ? '▼ Expand All' : '▶ Collapse All'}
+						{#if allCollapsed}
+							<svg
+								width="1em"
+								height="1em"
+								viewBox="0 0 24 24"
+								stroke-width="1"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								style="display: inline-block; margin-right: 0.5em;"
+								><path
+									fill-rule="evenodd"
+									clip-rule="evenodd"
+									d="M5.30711 8.71299C5.4232 8.43273 5.69668 8.25 6.00002 8.25H18C18.3034 8.25 18.5768 8.43273 18.6929 8.71299C18.809 8.99324 18.7449 9.31583 18.5304 9.53033L12.5304 15.5303C12.2375 15.8232 11.7626 15.8232 11.4697 15.5303L5.46969 9.53033C5.25519 9.31583 5.19103 8.99324 5.30711 8.71299Z"
+									fill="currentColor"
+								></path></svg
+							>
+						{:else}
+							<svg
+								width="1em"
+								height="1em"
+								viewBox="0 0 24 24"
+								stroke-width="1"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								style="display: inline-block; margin-right: 0.5em;"
+								><path
+									fill-rule="evenodd"
+									clip-rule="evenodd"
+									d="M8.71299 18.6929C8.43273 18.5768 8.25 18.3033 8.25 18V5.99998C8.25 5.69663 8.43273 5.42315 8.71299 5.30707C8.99324 5.19098 9.31583 5.25515 9.53033 5.46965L15.5303 11.4696C15.8232 11.7625 15.8232 12.2374 15.5303 12.5303L9.53033 18.5303C9.31583 18.7448 8.99324 18.809 8.71299 18.6929Z"
+									fill="currentColor"
+								></path></svg
+							>
+						{/if}
+						{allCollapsed ? 'Expand All' : 'Collapse All'}
 					</button>
 				</div>
 
@@ -344,7 +529,39 @@
 				<div class="engine">
 					<button class="header" onclick={() => (isEngineCollapsed = !isEngineCollapsed)}>
 						<div class="title">⚙️ Engine</div>
-						<span class="collapse-arrow">{isEngineCollapsed ? '▶' : '▼'}</span>
+						<span class="collapse-arrow">
+							{#if isEngineCollapsed}
+								<svg
+									width="9px"
+									height="9px"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M8.71299 18.6929C8.43273 18.5768 8.25 18.3033 8.25 18V5.99998C8.25 5.69663 8.43273 5.42315 8.71299 5.30707C8.99324 5.19098 9.31583 5.25515 9.53033 5.46965L15.5303 11.4696C15.8232 11.7625 15.8232 12.2374 15.5303 12.5303L9.53033 18.5303C9.31583 18.7448 8.99324 18.809 8.71299 18.6929Z"
+										fill="currentColor"
+									></path></svg
+								>
+							{:else}
+								<svg
+									width="9px"
+									height="9px"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M5.30711 8.71299C5.4232 8.43273 5.69668 8.25 6.00002 8.25H18C18.3034 8.25 18.5768 8.43273 18.6929 8.71299C18.809 8.99324 18.7449 9.31583 18.5304 9.53033L12.5304 15.5303C12.2375 15.8232 11.7626 15.8232 11.4697 15.5303L5.46969 9.53033C5.25519 9.31583 5.19103 8.99324 5.30711 8.71299Z"
+										fill="currentColor"
+									></path></svg
+								>
+							{/if}
+						</span>
 					</button>
 					<div class="content" class:collapsed={isEngineCollapsed}>
 						<div class="grid">
@@ -372,7 +589,39 @@
 				<div class="inventory">
 					<button class="header" onclick={() => (isInventoryCollapsed = !isInventoryCollapsed)}>
 						<div class="title">🎒 Inventory</div>
-						<span class="collapse-arrow">{isInventoryCollapsed ? '▶' : '▼'}</span>
+						<span class="collapse-arrow">
+							{#if isInventoryCollapsed}
+								<svg
+									width="9px"
+									height="9px"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M8.71299 18.6929C8.43273 18.5768 8.25 18.3033 8.25 18V5.99998C8.25 5.69663 8.43273 5.42315 8.71299 5.30707C8.99324 5.19098 9.31583 5.25515 9.53033 5.46965L15.5303 11.4696C15.8232 11.7625 15.8232 12.2374 15.5303 12.5303L9.53033 18.5303C9.31583 18.7448 8.99324 18.809 8.71299 18.6929Z"
+										fill="currentColor"
+									></path></svg
+								>
+							{:else}
+								<svg
+									width="9px"
+									height="9px"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M5.30711 8.71299C5.4232 8.43273 5.69668 8.25 6.00002 8.25H18C18.3034 8.25 18.5768 8.43273 18.6929 8.71299C18.809 8.99324 18.7449 9.31583 18.5304 9.53033L12.5304 15.5303C12.2375 15.8232 11.7626 15.8232 11.4697 15.5303L5.46969 9.53033C5.25519 9.31583 5.19103 8.99324 5.30711 8.71299Z"
+										fill="currentColor"
+									></path></svg
+								>
+							{/if}
+						</span>
 					</button>
 
 					<div class="content" class:collapsed={isInventoryCollapsed}>
@@ -407,7 +656,39 @@
 				<div class="equipment">
 					<button class="header" onclick={() => (isEquipmentCollapsed = !isEquipmentCollapsed)}>
 						<div class="title">🛠️ Equipment</div>
-						<span class="collapse-arrow">{isEquipmentCollapsed ? '▶' : '▼'}</span>
+						<span class="collapse-arrow">
+							{#if isEquipmentCollapsed}
+								<svg
+									width="9px"
+									height="9px"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M8.71299 18.6929C8.43273 18.5768 8.25 18.3033 8.25 18V5.99998C8.25 5.69663 8.43273 5.42315 8.71299 5.30707C8.99324 5.19098 9.31583 5.25515 9.53033 5.46965L15.5303 11.4696C15.8232 11.7625 15.8232 12.2374 15.5303 12.5303L9.53033 18.5303C9.31583 18.7448 8.99324 18.809 8.71299 18.6929Z"
+										fill="currentColor"
+									></path></svg
+								>
+							{:else}
+								<svg
+									width="9px"
+									height="9px"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M5.30711 8.71299C5.4232 8.43273 5.69668 8.25 6.00002 8.25H18C18.3034 8.25 18.5768 8.43273 18.6929 8.71299C18.809 8.99324 18.7449 9.31583 18.5304 9.53033L12.5304 15.5303C12.2375 15.8232 11.7626 15.8232 11.4697 15.5303L5.46969 9.53033C5.25519 9.31583 5.19103 8.99324 5.30711 8.71299Z"
+										fill="currentColor"
+									></path></svg
+								>
+							{/if}
+						</span>
 					</button>
 					<div class="content" class:collapsed={isEquipmentCollapsed}>
 						<div class="grid">
@@ -436,7 +717,39 @@
 				<div class="upgrades">
 					<button class="header" onclick={() => (isUpgradesCollapsed = !isUpgradesCollapsed)}>
 						<div class="title">⚙️ Upgrades</div>
-						<span class="collapse-arrow">{isUpgradesCollapsed ? '▶' : '▼'}</span>
+						<span class="collapse-arrow">
+							{#if isUpgradesCollapsed}
+								<svg
+									width="9px"
+									height="9px"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M8.71299 18.6929C8.43273 18.5768 8.25 18.3033 8.25 18V5.99998C8.25 5.69663 8.43273 5.42315 8.71299 5.30707C8.99324 5.19098 9.31583 5.25515 9.53033 5.46965L15.5303 11.4696C15.8232 11.7625 15.8232 12.2374 15.5303 12.5303L9.53033 18.5303C9.31583 18.7448 8.99324 18.809 8.71299 18.6929Z"
+										fill="currentColor"
+									></path></svg
+								>
+							{:else}
+								<svg
+									width="9px"
+									height="9px"
+									viewBox="0 0 24 24"
+									stroke-width="1"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M5.30711 8.71299C5.4232 8.43273 5.69668 8.25 6.00002 8.25H18C18.3034 8.25 18.5768 8.43273 18.6929 8.71299C18.809 8.99324 18.7449 9.31583 18.5304 9.53033L12.5304 15.5303C12.2375 15.8232 11.7626 15.8232 11.4697 15.5303L5.46969 9.53033C5.25519 9.31583 5.19103 8.99324 5.30711 8.71299Z"
+										fill="currentColor"
+									></path></svg
+								>
+							{/if}
+						</span>
 					</button>
 					<div class="content" class:collapsed={isUpgradesCollapsed}>
 						<div class="grid">
@@ -465,7 +778,26 @@
 						</div>
 					</div>
 				</div>
-				<button class="close-mobile" onclick={() => (isMobileSidebarOpen = false)}>✕</button>
+				<button
+					class="close-mobile"
+					onclick={() => (isMobileSidebarOpen = false)}
+					aria-label="Close"
+					><svg
+						width="1em"
+						height="1em"
+						stroke-width="1"
+						viewBox="0 0 24 24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426"
+							stroke="currentColor"
+							stroke-width="1"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						></path></svg
+					></button
+				>
 			</div>
 		</div>
 
@@ -601,7 +933,23 @@
 					></div>
 
 					<div class="sidebar" class:open={showRightSidebar}>
-						<button onclick={closeRightSidebar} class="close">✕</button>
+						<button onclick={closeRightSidebar} class="close" aria-label="Close"
+							><svg
+								width="1em"
+								height="1em"
+								stroke-width="1"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								><path
+									d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path></svg
+							></button
+						>
 						{#if selectedItem === 'dataShards'}
 							<div class="detail">
 								<div class="container">
@@ -622,7 +970,23 @@
 
 				<div class="drawer" class:visible={isDrawerExpanded}>
 					<div class="toolbar">
-						<button onclick={() => (isDrawerExpanded = false)} class="close">✕</button>
+						<button onclick={() => (isDrawerExpanded = false)} class="close" aria-label="Close"
+							><svg
+								width="1em"
+								height="1em"
+								stroke-width="1"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								><path
+									d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426"
+									stroke="currentColor"
+									stroke-width="1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								></path></svg
+							></button
+						>
 					</div>
 
 					<div class="detail">
@@ -646,7 +1010,33 @@
 					<!-- <button class="hamburger" onclick={() => (isMobileSidebarOpen = !isMobileSidebarOpen)}>
 						☰
 					</button> -->
-					☰
+					<svg
+						width="1em"
+						height="1em"
+						stroke-width="1"
+						viewBox="0 0 24 24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M3 5H21"
+							stroke="currentColor"
+							stroke-width="1"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						></path><path
+							d="M3 12H21"
+							stroke="currentColor"
+							stroke-width="1"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						></path><path
+							d="M3 19H21"
+							stroke="currentColor"
+							stroke-width="1"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						></path></svg
+					>
 				</button>
 				<div class="content">Footer.</div>
 				<div class="drawer" class:visible={isFooterExpanded}>
