9276b2321b0205131f44f034e25ed65ed755c528
Kashinoga <kashinoga@Mac.lan>
2025-12-17 10:11:47 -0600

    sidebar and button standardization

---
+++ project-overview.md
@@ -0,0 +1,40 @@
+# Views
+
+## Locations
+
+1. Home
+   1. Blue
+2. Basecamp
+   1. Green
+3. Starship
+   1. Gold
+
+## Resources
+
+1. Ranger
+   1. Orange
+2. Inventory
+   1. Leather
+3. Equipment
+   1. Black
+
+## Communications
+
+1. Email
+   1. Blue
+2. Calendar
+   1. Red
+
+## Skills
+
+1. Sheltering
+   1. Brown
+2. Woodcutting
+   1. Green
+
+## Miscellaneous
+
+1. Developer
+   1. Purple
+2. Settings
+   1. Silver
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import lightIcon from '$lib/assets/favicon-light.svg';
+	import darkIcon from '$lib/assets/favicon-dark.svg';
 	import '../app.css';
 	import '../app.ts';
 
@@ -7,7 +8,9 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={lightIcon} type="image/svg+xml" media="(prefers-color-scheme: light)" />
+	<link rel="icon" href={darkIcon} type="image/svg+xml" media="(prefers-color-scheme: dark)" />
+	<link rel="icon" href={lightIcon} type="image/svg+xml" />
 </svelte:head>
 
 {@render children()}
